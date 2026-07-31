ee5e227603ba5782d41aa200fc1db4dc40918f18
Izarot <sharananandanaraizarot@gmail.com>
2026-07-31 15:39:02 +0530

    a snull fex

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+const express = require('express');
+// ... rest of the code
+
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
